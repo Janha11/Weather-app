c15920b0eed6c81fd2a7c7cfa7103730f2ed82d8
Janha11 <janha11062001@gmail.com>
2024-01-10 18:37:22 +0530

    Completed UI for Sidebar

---
+++ sidebar.js
@@ -0,0 +1,15 @@
+const sidebar = `
+    <div class='wrapper'>
+        <div class='sidebar'>
+            <div>
+                <form action='' class='search' id='search'>
+                    <input type='text' id='query' placeholder='Search'/>
+                    <button>
+                    ${'hello' }</button>
+                </form>
+            </div>
+        </div>
+    </div>
+`;
+
+export default sidebar;
